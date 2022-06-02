b5f9f92f358f1ec3d494f362c54a9a03b99eaffa
Lincoln Oliveira <legionario_172@hotmail.com>
2022-06-02 20:20:49 -0300

    Update script iam aws.py

--- AWS/script iam aws.py
+++ AWS/script iam aws.py
@@ -36,6 +36,7 @@ def criarUsuario(iam_resource):
         for response in user.paginate():
 ...     print(response)
 ...
+									       
 #Alterando usuário:
 def atualizarUsuario(iam_resource):
     nome = input("Informe o nome do usuário que deseja alterar: ")
@@ -46,6 +47,7 @@ def atualizarUsuario(iam_resource):
 def deletandoUsuario(iam_resource)
     usuario = input("Informe o nome do usuário que deseja deletar: ")
     iam.delete_user(UserName=usuario) 
+									       
 #Regra para atribuir politica aos usuários:
 
 try:
@@ -64,17 +66,22 @@ try:
         raise
    
 #Criando politicas de acesso: 
+def leitura():
+  									       
 #Politica de acesso somente leitura
 policy_readOnly = {
 	"Version": "2022-05-27"
 	"Statement": {
 			"Effect":  "Allow",
 			"Action":  ["iam:Get*", "iam:List*"],
-			"Resource": ["arn:aws:iam::*:user/${aws:username}"]
-            role.attach_policy(PolicyArn=policy.arn)
+			"Resource": ["arn:aws:iam::*:user/${aws:username}"],
+            		role.attach_policy(PolicyArn=policy.arn)
 	}
 }
+raise
+									       
 #Politica de acesso leitura e escrita
+def readWrite():									       
 policy_read_write = {
 	"Version": "2022-05-27"
 	"Statement": {
@@ -84,21 +91,53 @@ policy_read_write = {
 		"Resource": ["arn:aws:iam::*:user/${aws:username}"]
 	}
 }
+raise									       
+									       
 #Politica de acesso admin
+def admin():									       
 Policy_admin_aws = {
 	"Version": "2022-05-28"
 	"Statement": {
 		"Effect": "Allow"
 		"Action": "iam:*",
-		"Resource": "*"
+		"Resource": ["arn:aws:iam::*:user/${aws:username}"]
 	}
 }
-while == true {
-	print("Selecione uma opção: ")
-	print("1 - Criar usuário")
-	print("2 - Pesquisar usuário")
-	print("3 - Alterar usuário")
-	print("4 - Deletar usuário")
-	print("5 - atribuir uma politica ao usuário")
-	print("6 - Sair")
+raise
 
+def escolhePolitica():
+username = input("Informe o nome do usuário para atribuir a politica: ")
+print("Defina o tipo de política para o usuário: ")
+print("1 - Somente Leitura")
+print("2 - Leitura e escrita")									       
+print("3 - Administrador")
+
+escolha = input()
+if escolha == 1:
+	return leitura(username)
+elif escolha == 2:									       
+	return 	readWrite(username)								       
+elif escolhe == 3:									       
+	return admin(username)
+else:
+	return print("Erro! Escolha entre 1, 2 ou 3")
+raise
+									       
+if __name__ == "__main__":									       
+	while == true {
+		print("Selecione uma opção: ")
+		print("1 - Criar usuário")
+		print("2 - Pesquisar usuário")
+		print("3 - Alterar usuário")
+		print("4 - Deletar usuário")
+		print("5 - atribuir uma politica ao usuário")
+		print("6 - Sair")
+		
+		switch {
+			"1": criarUsuario,
+			"2": listarUsuario,
+			"3": atualizarUsuario,
+			"4": deletandoUsuario,
+			"5" escolhePolitica,
+			"6" exit()
+	break
